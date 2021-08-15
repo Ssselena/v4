fc4d74fbd0c10c72132f58190ba3566c7adf08c5
Ssselena <32481347+Ssselena@users.noreply.github.com>
2021-08-15 23:18:39 +1000

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,16 +59,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Julia Drugova.</h2>;
+  const three = <h3 className="big-heading">I design user experiences.</h3>;
   const four = (
     <p>
-      I'm a software engineer specializing in building (and occasionally designing) exceptional
-      digital experiences. Currently, I'm focused on building accessible, human-centered products at{' '}
-      <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-        Upstatement
-      </a>
-      .
+      I'm a UX/UI deisgner (and occasionally developing) exceptional
+      digital experiences. Currently, I'm focused on building accessible, human-centered products.
     </p>
   );
   const five = (
